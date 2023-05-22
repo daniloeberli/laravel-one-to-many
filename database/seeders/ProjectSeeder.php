426e12956f67de2afa1403e5c984571eed12713d
daniloeberli <danilo.eberli@gmail.com>
2023-05-22 16:39:32 +0200

    feat: type entity added

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -17,13 +18,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 0; $i < 5; $i++) {
-            $project = new Project();
+        Project::truncate();
 
+        for($i = 0; $i < 20; $i++) {
+            $type = Type::inRandomOrder()->first();
+            $project = new Project();
             $project->title = $faker->sentence(4);
             $project->description = $faker->text(200);
             $project->stack = $faker->text(50);
             $project->slug = Str::slug($project->title);
+            $project->type_id = $type->id;
             $project->save();
         }
     }
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['exercise','group project','personal project','final project'];
+
+        Schema::disableForeignKeyConstraints();
+        Type::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        foreach($types as $type){
+            
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name);
+            $new_type->save();
+        }
+    }
+}
