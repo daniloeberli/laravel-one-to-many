5025cfe2543e8323113c5f2e0b411cebeecfe873
daniloeberli <danilo.eberli@gmail.com>
2023-05-22 18:13:19 +0200

    feat: type delete method

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -83,6 +83,7 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+        return to_route('admin.types.index');
     }
 }
